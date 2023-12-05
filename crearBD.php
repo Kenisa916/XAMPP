f16958aba1a0634db141b4083c072a5528655bf3
Kenisa916 <kevnicsan@alu.edu.gva.es>
2023-12-05 12:53:14 +0100

    Crear

---
+++ crearBD.php
@@ -0,0 +1,15 @@
+<?php
+
+include("Conexión.php");
+
+
+// Create database
+$sql = "CREATE DATABASE myDB";
+if (mysqli_query($conn, $sql)) {
+  echo "Database created successfully";
+} else {
+  echo "Error creating database: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
